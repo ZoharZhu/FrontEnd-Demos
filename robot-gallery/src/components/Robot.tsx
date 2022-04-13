1d6c9818c4c396e5ef15cf7596f8b9c95ae22c78
Zzh-Zh3nDu1 <530178528@qq.com>
2022-04-13 20:22:10 +0800

    add: appState.tsx

--- robot-gallery/src/components/Robot.tsx
+++ robot-gallery/src/components/Robot.tsx
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import styles from './Robot.module.css'
-import { appContext } from "../index"
+import { appContext, appSetStateContext } from "../AppState";
 
 interface RobotProps {
   id: number,
@@ -12,12 +12,27 @@ interface RobotProps {
 // (props) => { const id = props.id ...}
 const Robot: React.FC<RobotProps> = ({ id, name, email }) => {
   const value = useContext(appContext)
+  const setState = useContext(appSetStateContext)
+  const addToCart = () => {
+    if(setState) {
+      setState(state => {
+        return {
+          ...state,
+          shoppingCart: {
+            items: [...state.shoppingCart.items, {id, name}]
+          }
+        }
+      })
+    }
+  }
+
   return (
     <div className={styles.cardContainer}>
       <img alt="robot" src={`https://robohash.org/${id}`} />
       <h2>{name}</h2>
       <p>{email}</p>
       <p>作者：{value.username}</p>
+      <button onClick={addToCart}>加入购物车</button>
     </div>
   )
 }
--- robot-gallery/src/index.tsx
+++ robot-gallery/src/index.tsx
@@ -3,18 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AppStateProvider } from './AppState';
 
-const defaultContextValue = {
-  username: "ZoharZhu"
-}
-
-export const appContext = React.createContext(defaultContextValue)
 
 ReactDOM.render(
   <React.StrictMode>
-    <appContext.Provider value={defaultContextValue}>
+    <AppStateProvider>
       <App />
-    </appContext.Provider>
+    </AppStateProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
